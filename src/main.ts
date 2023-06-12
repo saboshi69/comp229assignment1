24d0331f3a9a7d1ef78b358b1bf83d3b81554fa2
saboshi69 <shui2967@gmail.com>
2023-06-11 20:56:16 -0400

    add comment for main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+// File: main.ts
+// Student's Name: Wan Kei Hui
+// Student ID: 301266228
+// Date: 11-6-2023
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
